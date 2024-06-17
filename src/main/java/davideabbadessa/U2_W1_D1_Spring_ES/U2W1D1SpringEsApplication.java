713c1f15a9d1c1a267489de125e65208fb89f157
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-17 14:21:24 +0200

    .

---
+++ src/main/java/davideabbadessa/U2_W1_D1_Spring_ES/U2W1D1SpringEsApplication.java
@@ -0,0 +1,13 @@
+package davideabbadessa.U2_W1_D1_Spring_ES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W1D1SpringEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W1D1SpringEsApplication.class, args);
+	}
+
+}
